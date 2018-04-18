6df655163c0c2cd4c6463157d8c1baa54e38461b
Luis Tejero <luisfaridta@gmail.com>
2018-04-18 12:02:26 +0100

    all functions before buying drink done

--- customer.rb
+++ customer.rb
@@ -10,4 +10,20 @@ class Customer
     @drunk_level = 0
   end
 
+  def add_drunk(drink)
+    @drunk_level += drink.alcohol_level
+  end
+
+  def can_afford(drink)
+    if @wallet >= drink.price
+      return true
+    else
+      return false
+    end
+  end
+
+  def subtract(drink)
+    @wallet -= drink.price
+  end
+
 end
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -12,11 +12,34 @@ class CustomerTest < MiniTest::Test
     @pub = Pub.new("The Chanter", [@drink1, @drink2])
 
     @customer = Customer.new("Luis", 4, 20)
+    @customer2 = Customer.new("Vader", 1, 20)
    end
 
    def test_customer_name
      assert_equal("Luis", @customer.name)
    end
 
+   def test_add_drunk
+     @customer.add_drunk(@drink1)
+     assert_equal(2, @customer.drunk_level)
+   end
+
+   def test_afford_drink__true
+     assert_equal(true, @customer.can_afford(@drink1))
+   end
+
+   def test_afford_drink__false
+     assert_equal(false, @customer2.can_afford(@drink1))
+   end
+
+   def test_subtract_wallet
+     @customer.subtract(@drink1)
+     assert_equal(2, @customer.wallet)
+   end
+
+   def test_subtract_wallet__more
+     @customer.subtract(@drink2)
+     assert_equal(1, @customer.wallet)
+   end
 
  end
